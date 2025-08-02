67d03aaa338d3537709fd736e37ddc206251cc5c
ndhung1992k7 <hungnguyenze9@gmail.com>
2025-08-02 18:28:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -88,3 +88,11 @@ setInterval(() => {
     }
   });
 });
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loadingScreen");
+  setTimeout(() => {
+    loader.style.opacity = "0";
+    loader.style.transition = "opacity 0.5s ease";
+    setTimeout(() => loader.remove(), 500);
+  }, 4000); // khớp với thời gian animation
+});
